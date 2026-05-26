ba7e2e65b1d0eee3520943a2b0f4ae433355586b
yannd38-coder <yann.dicko@gmail.com>
2026-05-26 11:26:50 +0200

    ajout eventlistener

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,30 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+//let baliseBanner = document.getElementById("banner");
+//console.log(baliseBanner);
+const flecheGauche = document.querySelector(".arrow_left");
+const flecheDroite = document.querySelector(".arrow_right");
+console.log(flecheGauche);
+console.log(flecheDroite);
+flecheGauche.addEventListener("click", () => {
+	console.log("Clique sur fleche de gauche")
+});
+flecheDroite.addEventListener("click", () => {
+	console.log("Clique sur fleche de droite")
+});
